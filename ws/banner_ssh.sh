30ababd7f6e013e88203405efb1aa971ef57c7e8
Xresx30k <takashimitsuki65@gmail.com>
2025-03-22 01:01:45 +0700

    Add files via upload

---
+++ ws/banner_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+cat > /etc/issue.net << END
+<font color="green">Nikmatin Kang....</font>
+<p style="text-align: center;">
+<font color="cyan"><b><big><big><big>XRESX DIGITAL VPN</font></b></big></big></big></p>
+<p style="text-align: center;">
+    <span style="color: #7FFF00;">Support us:</span>
+    <span style="color: #EFEFEF;">WA: wa.me/6285194758303</span>
+</p>
+END
